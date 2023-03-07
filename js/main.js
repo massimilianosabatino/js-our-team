d6867b40a2b7da66e473b8c6d3282aa7f3828d7f
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-03-07 15:13:13 +0100

    create array with object

---
+++ js/main.js
@@ -0,0 +1,50 @@
+'use strict';
+
+// Wayne Barnett	Founder & CEO	wayne-barnett-founder-ceo.jpg
+// Angela Caroll	Chief Editor	angela-caroll-chief-editor.jpg
+// Walter Gordon	Office Manager	walter-gordon-office-manager.jpg
+// Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+// Scott Estrada	Developer	scott-estrada-developer.jpg
+// Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg
+
+const members = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        photo: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        photo: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        photo: 'walter-gordon-office-manager.jpg',
+    },
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        photo: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        photo: 'scott-estrada-developer.jpg',
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        photo: 'barbara-ramos-graphic-designer.jpg',
+    }
+]
+
+//Stampa intero array
+console.log(members);
+//Stampa oggetto contenuto in posizione 0 dell'array
+console.log(members[0]);
+//Stampa valori di ogni chiave richiamata per l'oggetto in posizione 0 dell'array
+console.log(members[0].name);
+console.log(members[0].role);
+console.log(members[0].photo);
